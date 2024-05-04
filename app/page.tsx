9bb3b5126637a04a244049b63414eb922267befc
LucasRZamboni <lucasrzamboni@hotmail.com>
2024-05-03 23:58:17 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import  Search  from "./_components/search";
 import Header from "./_components/header"
 
 const Home = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <div className="px-5 py-6">
+      <Search />
+      </div>
+    </>
   );
 }
 
